7704946db2ee12643baf7cfe7014d811294f42b3
蓝天翔 <tianxianglan@126.com>
2017-01-06 12:36:45 +0800

    添加了第二题按ASCII码排序的代码

---
+++ HuaWei/code/RankByAsciiOfHW.java
@@ -0,0 +1,42 @@
+package interview;
+
+import java.util.Scanner;
+
+/**
+ * Created by tianxianglan on 2017/1/6.
+ */
+public class RankByAsciiOfHW {
+    public static void main(String[] args){
+        System.out.print("please input a string: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        char[] chars = str.toCharArray();
+        int[] indexOfChar = new int[str.length()];
+        int num = 0;
+        for (int i = 0; i< str.length(); i++){
+            char letter = str.charAt(i);
+            if ((letter> 'A' && letter< 'Z') || (letter> 'a' && letter< 'z')){
+                indexOfChar[num] = i;
+                num++;
+            }
+        }
+        num--;
+
+        //采用冒泡排序对字母进行升序排序
+        for (int j = 0; j< num; j++){
+            for (int k = 0; k< num- j; k++){
+                int kIndex = indexOfChar[k];
+                if (chars[kIndex] > chars[indexOfChar[k+ 1]]){
+                    char ch = chars[indexOfChar[k+ 1]];
+                    chars[indexOfChar[k+ 1]] = chars[kIndex];
+                    chars[kIndex] = ch;
+                }
+            }
+        }
+
+        System.out.print("string after rank: ");
+        for (int h = 0; h< str.length(); h++){
+            System.out.print(chars[h]);
+        }
+    }
+}
